61f1966e4c1725314db83e18dd8df77754765418
Joni.ikävalko <juhani2006@gmail.com>
2023-10-04 09:16:02 +0300

    Update Arcade_machine_UI CTK V1.py

--- Joni/Arcade_machine_UI CTK V1.py
+++ Joni/Arcade_machine_UI CTK V1.py
@@ -10,13 +10,17 @@ customtkinter.set_default_color_theme("dark-blue")
 
 app = customtkinter.CTk()
 
+
+# define app Name and Window Resolution
 app.title("Testi")
 app.geometry("1920x1080")
 
+# Show value Function
 def show_value(selected_option):
     print(selected_option)
     launch_game(selected_option)
 
+# Launch Game Function
 def launch_game(game_name):
     exe_file = f"{game_name}.exe"
     if os.path.exists(exe_file):
@@ -24,6 +28,8 @@ def launch_game(game_name):
     else:
         print(f"Executable '{exe_file}' not found!")
 
+
+# List Box Object
 listbox = CTkListbox(app, command=show_value)
 listbox.pack(fill="both", expand=True, padx=400, pady=200)
 
@@ -36,7 +42,6 @@ for file_name in os.listdir(script_directory):
         game_name = os.path.splitext(file_name)[0]
         listbox.insert("end", game_name)
         executable_files.append(game_name)
-
 def browse_file():
     global executable_files
     root_dir = tkinter.filedialog.askdirectory()
@@ -45,9 +50,11 @@ def browse_file():
     for game_name in executable_files:
         listbox.insert("end", game_name)
 
-button = customtkinter.CTkButton(master=app, text="Pelaa", command=browse_file)
+#Button Object 
+button = customtkinter.CTkButton(master=app, text="Pelaa", command=launch_game)
 button.place(relx=0.5, rely=0.75, anchor=tkinter.CENTER)
 
+# Scans Exe FiLes
 def get_executable_files(root_dir):
     executable_files = []
     for root, dirs, files in os.walk(root_dir):
@@ -57,6 +64,7 @@ def get_executable_files(root_dir):
                 executable_files.append(game_name)
     return executable_files
 
+#FullScreen
 app.attributes("-fullscreen", "True")
 # Runs the app
 app.mainloop()
